895a220845d3fc37e2a60c28bc6290a34c1d469f
likhitaaluru <likhitaaluru@gmail.com>
2018-08-08 15:45:09 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -4,7 +4,8 @@ rainbow_colors = [
   "light_cyan"
 ]
 
-def change_rainbow_colors'
+def change_rainbow_colors
+  change_rainbow_colors = rainbow_colors
   rainbow_colors[0]= "red"
   rainbow_colors[1]= "light_red"
   rainbow_colors[2]= "light_yellow"
